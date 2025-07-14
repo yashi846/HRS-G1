61ab38a0cad4147d191b8484e1c472412a10a401
tomatoma-ui <tomatomatoma0526@outlook.jp>
2025-07-14 21:59:10 +0900

    Update Plan.java

--- Task5/プログラム/src/Plan.java
+++ Task5/プログラム/src/Plan.java
@@ -3,7 +3,6 @@ public abstract class Plan {
     protected int price;
 
     // クラス図から作成 - 価格取得
-    // AIによる実装のためロジックが本来の意図と異なる可能性あり
     // プランの価格を返却する
     public int getPrice() {
         return price;
