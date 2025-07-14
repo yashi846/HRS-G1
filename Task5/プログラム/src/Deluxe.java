90a0007b6bbee715493157c5a77438e9744b58e9
tomatoma-ui <tomatomatoma0526@outlook.jp>
2025-07-14 22:05:53 +0900

    Update Deluxe.java

--- Task5/プログラム/src/Deluxe.java
+++ Task5/プログラム/src/Deluxe.java
@@ -1,6 +1,5 @@
 public class Deluxe extends Grade {
     // クラス図から作成 - コンストラクタ
-    // AIによる実装のためロジックが本来の意図と異なる可能性あり
     // Gradeクラスの継承によりデラックスルームの価格と空室数を設定
     public Deluxe(int price, int vacantRoomNum) {
         this.price = price;
