adebf4c3fdf87fe72a63ac695ce5a902170d210b
tomatoma-ui <tomatomatoma0526@outlook.jp>
2025-07-14 21:57:59 +0900

    Update PlanC.java

--- Task5/プログラム/src/PlanC.java
+++ Task5/プログラム/src/PlanC.java
@@ -1,6 +1,5 @@
 public class PlanC extends Plan {
     // クラス図から作成 - コンストラクタ
-    // AIによる実装のためロジックが本来の意図と異なる可能性あり
     // Planクラスの継承により価格を設定
     public PlanC(int price) {
         this.price = price;
